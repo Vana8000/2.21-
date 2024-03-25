032ba81d5fc06cedc6b9ca6d55b7d889b93e2c02
Vana8000 <163187974+Vana8000@users.noreply.github.com>
2024-03-25 20:00:36 +0300

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите строку:");
+        String input = scanner.nextLine();
+
+        input = input.replace("кака", "вырезано цензурой");
+        input = input.replace("бяка", "вырезано цензурой");
+
+        System.out.println("Результат замены: " + input);
+    }
+}
